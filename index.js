c07623d0e7539a3a1cf2f1f40ca53f26854319f4
goodwyyn <godwin.mtuma@student.moringaschool.com>
2022-06-13 12:37:27 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(initial){
+    let distance = 42;
+    if(initial>distance){
+        return(initial-distance);
+    }else{
+        return(distance-initial)
+    }
+}
+function distanceFromHqInFeet(initial){
+    return distanceFromHqInBlocks (initial) * 264
+    
+    
+}
+
+
+function distanceTravelledInFeet(start, destination){
+    if (start < destination){
+        return (destination - start) * 264
+        
+    }
+    else { 
+        return (start - destination) * 264
+    }
+}
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start,  destination)
+    if (distance <= 400) { 
+        return 0;
+    }
+    else if (distance > 400 && distance <= 2000){
+        return .02 *(distance - 400)
+    } else if (distance > 2000 && distance < 2500){
+        return 25;
+    } else {
+        return 'cannot travel that far'
+    }
+}
